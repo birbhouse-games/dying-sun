d53f1736c12e1285ef803ab72d99b5c981468627
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-12-12 16:28:39 -0500

    fix: filtering by key binding no longer mistakes 0 for undefined

--- src/hooks/useKeyboardStateSystem.ts
+++ src/hooks/useKeyboardStateSystem.ts
@@ -1,82 +1,68 @@
 // Module imports
-import {
-  useCallback,
-  useEffect,
-} from 'react'
-
-
-
-
+import { useCallback, useEffect } from "react";
 
 // Local imports
-import { KEY_BINDINGS } from '@/constants/KEY_BINDINGS.ts'
-import { store } from '@/store/store.ts'
-
-
-
-
+import { KEY_BINDINGS } from "@/constants/KEY_BINDINGS.ts";
+import { store } from "@/store/store.ts";
 
 export function useKeyboardStateSystem() {
-  const handleKeyDown = useCallback((event: KeyboardEvent) => {
-		if (!KEY_BINDINGS[event.key]) {
-			return
-    }
+	const handleKeyDown = useCallback((event: KeyboardEvent) => {
+		if (KEY_BINDINGS[event.key] === undefined) {
+			return;
+		}
 
-    event.preventDefault()
+		event.preventDefault();
 
-		const { keyboardState } = store.state
-		let keyState = keyboardState.get(event.key)
+		const { keyboardState } = store.state;
+		let keyState = keyboardState.get(event.key);
 
 		if (!keyState) {
 			keyState = {
 				activatedAt: null,
 				deactivatedAt: null,
 				isActive: false,
-				sinceLastActivated: null
-			}
-			keyboardState.set(event.key, keyState)
+				sinceLastActivated: null,
+			};
+			keyboardState.set(event.key, keyState);
 		}
 
-    if (keyState.isActive) {
-      return
-    }
+		if (keyState.isActive) {
+			return;
+		}
 
-    keyState.activatedAt = store.state.now
-    keyState.isActive = true
+		keyState.activatedAt = store.state.now;
+		keyState.isActive = true;
 
-    store.set(() => ({ keyboardState: new Map(keyboardState) }))
-  }, [])
+		store.set(() => ({ keyboardState: new Map(keyboardState) }));
+	}, []);
 
-  const handleKeyUp = useCallback((event: KeyboardEvent) => {
-		if (!KEY_BINDINGS[event.key]) {
-			return
-    }
+	const handleKeyUp = useCallback((event: KeyboardEvent) => {
+		if (KEY_BINDINGS[event.key] === undefined) {
+			return;
+		}
 
-    event.preventDefault()
+		event.preventDefault();
 
-		const { keyboardState } = store.state
-		const keyState = keyboardState.get(event.key)
+		const { keyboardState } = store.state;
+		const keyState = keyboardState.get(event.key);
 
-    if (!keyState) {
-      return
-    }
+		if (!keyState) {
+			return;
+		}
 
-    keyState.deactivatedAt = store.state.now
-    keyState.isActive = false
+		keyState.deactivatedAt = store.state.now;
+		keyState.isActive = false;
 
-    store.set(() => ({ keyboardState: new Map(keyboardState) }))
-  }, [])
+		store.set(() => ({ keyboardState: new Map(keyboardState) }));
+	}, []);
 
-  useEffect(() => {
-    document.addEventListener('keydown', handleKeyDown)
-    document.addEventListener('keyup', handleKeyUp)
+	useEffect(() => {
+		document.addEventListener("keydown", handleKeyDown);
+		document.addEventListener("keyup", handleKeyUp);
 
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown)
-      document.removeEventListener('keyup', handleKeyUp)
-    }
-  }, [
-    handleKeyDown,
-    handleKeyUp,
-  ])
+		return () => {
+			document.removeEventListener("keydown", handleKeyDown);
+			document.removeEventListener("keyup", handleKeyUp);
+		};
+	}, [handleKeyDown, handleKeyUp]);
 }
